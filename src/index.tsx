6de451b8b0279408aae2010e9f6551cea10d8b62
Yixuan Xu <yixuanxu94@gmail.com>
2019-06-26 01:08:51 +0800

    feat((animation)): :sparkles: Pull to refresh finished
    TODO: Infinity scroll

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,166 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect, useReducer } from 'react';
 import { clamp } from 'lodash';
 import { useSpring, animated } from 'react-spring';
 import { useGesture } from 'react-with-gesture';
+import ReactLoading from 'react-loading';
+import { VariableSizeList as List } from 'react-window';
+
+// These row heights are arbitrary.
+// Yours should be based on the content of the row.
+const rowHeights = new Array(1000)
+  .fill(true)
+  .map(() => 25 + Math.round(Math.random() * 50));
+
+const getItemSize = (index: number) => rowHeights[index];
+
+const Row = ({
+  index,
+  style,
+}: {
+  index: number;
+  style: React.CSSProperties;
+}) => <div style={style}>Row {index}</div>;
 
 const refresh = () =>
   new Promise(resolve => {
     setTimeout(() => {
-      resolve();
+      resolve(true);
     }, 5000);
   });
+
+type InitialState = {
+  width: number;
+  height: number;
+};
+type Action = {
+  type: 'UPDATE' | 'RESET';
+  playLoad: InitialState;
+};
+const initialState: InitialState = { width: 0, height: 0 };
+const reducer = (state: InitialState, action: Action) => {
+  switch (action.type) {
+    case 'UPDATE':
+      return action.playLoad;
+    case 'RESET':
+      return { width: 0, height: 0 };
+    default:
+      return state;
+  }
+};
 export function Thing() {
   const [{ y }, set] = useSpring(() => ({ y: 0 }));
+  const [{ op }, setop] = useSpring(() => ({ op: 0 }));
   const loading = useRef(false);
+  const listContainerRef = useRef<HTMLDivElement>(null);
   const [state, setstate] = useState(false);
+  const [size, dispatch] = useReducer(reducer, initialState);
+  const [text, settext] = useState('Pull to refresh');
+  useEffect(() => {
+    if (listContainerRef.current) {
+      const { offsetWidth, offsetHeight } = listContainerRef.current;
+      dispatch({
+        type: 'UPDATE',
+        playLoad: { width: offsetWidth, height: offsetHeight },
+      });
+    }
+  }, []);
+  useEffect(() => {
+
+  })
   const bind = useGesture(({ down, delta, velocity }) => {
     velocity = clamp(velocity, 1, 8);
-    set({
-      y: down ? delta[1] : 100,
-      config: { mass: velocity, tension: 500 * velocity, friction: 50 },
-    });
-    if (!loading.current) {
-      loading.current = true;
-      setstate(loading.current);
-      console.log('queue');
-      refresh().then(() => {
-        console.log('wtf');
-        loading.current = false;
-        setstate(loading.current);
+    if (down) {
+      if (delta[1] <= 75) {
         set({
-          y: 0,
+          y: delta[1],
           config: { mass: velocity, tension: 500 * velocity, friction: 50 },
         });
+        if (delta[1] > 70) {
+          settext('松开刷新');
+        } else {
+          if (delta[1] >= 25) {
+            setop({
+              op: delta[1] / 25 - 1,
+            });
+          }
+        }
+      }
+    } else {
+      setop({
+        op: 0,
       });
+      if (!loading.current) {
+        if (delta[1] > 70) {
+          loading.current = true;
+          setstate(loading.current);
+          set({
+            y: 50,
+            config: { mass: velocity, tension: 500 * velocity, friction: 50 },
+          });
+          refresh().then(() => {
+            loading.current = false;
+            setstate(loading.current);
+            settext('Release to refresh');
+            set({
+              y: 0,
+              config: { mass: velocity, tension: 500 * velocity, friction: 50 },
+            });
+          });
+        } else {
+          set({
+            y: 0,
+            config: { mass: velocity, tension: 500 * velocity, friction: 50 },
+          });
+        }
+      }
     }
   });
   return (
     <>
-      <div>{state.toString()}</div>
+      <div
+        className="loading"
+        style={{
+          transform: `translateY(-25px)`,
+        }}
+      >
+        {state ? (
+          <ReactLoading
+            type="spokes"
+            color="#000"
+            height={25}
+            width={25}
+          ></ReactLoading>
+        ) : (
+            <animated.div
+              className="tips"
+              style={{
+                opacity: op.interpolate(o => o),
+                transform: y.interpolate(
+                  (y: number) => `translateY(${-50 + y}px)`
+                ),
+              }}
+            >
+              {text}
+            </animated.div>
+          )}
+      </div>
       <animated.div
-        className="ball"
+        className="List"
+        ref={listContainerRef}
         {...bind()}
         style={{
-          transform: y.interpolate((y: number) => `translateY(${y}px)`),
+          transform: y.interpolate((y: number) => `translateY(${-100 + y}px)`),
         }}
-      />
+      >
+        <List
+          height={size.height}
+          itemCount={1000}
+          itemSize={getItemSize}
+          width={size.width}
+        >
+          {Row}
+        </List>
+      </animated.div>
     </>
   );
 }
